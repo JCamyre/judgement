a442c096df7a98076d37c38cedaec1af55b38c4d
JCamyre <jwcamry03@gmail.com>
2024-10-03 12:29:16 -0700

    Add Alma document pairs. Add basic LLM-generated criteria.

---
+++ judgement/eval/llm_gen_criteria.py
@@ -0,0 +1,44 @@
+import os
+import pydantic
+
+from judgement import langfuse
+from judgement.constants import GPT4_MINI
+from judgement.prompt_names import LLM_GEN_CRITIERIA
+from judgement.data.cleaning.utils import read_file, get_chat_completion
+
+class CriteriaOutput(pydantic.BaseModel):
+    CRITERIA: str
+    THOUGHTS: str
+
+def generate_criteria(rough_draft_path: str, final_draft_path: str, model_type: str = GPT4_MINI, response_format: str = CriteriaOutput):
+    prompt = langfuse.get_prompt(
+        LLM_GEN_CRITIERIA,
+        type="chat"
+    )
+    
+    # Load document pair
+    rough_draft = read_file(rough_draft_path)
+        
+    final_draft = read_file(final_draft_path)
+    
+    criteria = None
+        
+    raw_prompt = f"Rough draft: {rough_draft}\nFinal draft: {final_draft}\Current criteria: {criteria}"
+    
+    compiled_prompt = prompt.compile(
+        criteria=raw_prompt
+    )
+    
+    # Format the response into two parts: the explanation for everything, and the criteria
+    
+    
+    return get_chat_completion(model_type, compiled_prompt, response_format=response_format)
+    
+
+if __name__ == "__main__":
+    # Testing
+    rough_draft_path = os.path.join(os.path.dirname(__file__), "documents/alma_anonymized_draft", "243711d2cc.txt")
+    final_draft_path = os.path.join(os.path.dirname(__file__), "documents/alma_anonymized_final", "243711d2cc.txt")
+    print(generate_criteria(rough_draft_path, final_draft_path))
+    print("*" * 50)
+    
