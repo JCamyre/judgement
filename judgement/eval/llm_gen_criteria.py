6919e011c4d3983471fff38e3939539e75ccb4f3
JCamyre <jwcamry03@gmail.com>
2024-10-04 18:23:10 -0700

    .

--- judgement/eval/llm_gen_criteria.py
+++ judgement/eval/llm_gen_criteria.py
@@ -10,8 +10,15 @@ class CriteriaOutput(pydantic.BaseModel):
     CRITERIA: str
     THOUGHTS: str
 
-# TODO: Append the criteria to the next LLM call. Have to update the prompt to inform LLM about it.
-def generate_criteria(rough_draft_path: str, final_draft_path: str, model_type: str = GPT4_MINI, response_format: str = CriteriaOutput):
+# Have it give examples for the criteria, once we aggregate all the criteria, it can have hard examples of 1, 3, 5 for each criteria
+# TODO: Learn more about what LLM as a judge likes for criteria
+# Alignment can come later
+# How can I use the "thoughts" to help
+# Apply more prompt engineering techniques
+# "such that we can help immigrants enter the US"
+# TODO Small write up on my prompting
+
+def generate_criteria(rough_draft_path: str, final_draft_path: str, criteria: str = "", model_type: str = GPT4_MINI, response_format: str = CriteriaOutput):
     prompt = langfuse.get_prompt(
         LLM_GEN_CRITIERIA,
         type="chat"
@@ -21,8 +28,6 @@ def generate_criteria(rough_draft_path: str, final_draft_path: str, model_type: 
     rough_draft = read_file(rough_draft_path)
         
     final_draft = read_file(final_draft_path)
-    
-    criteria = None
         
     raw_prompt = f"Rough draft: {rough_draft}\nFinal draft: {final_draft}\Current criteria: {criteria}"
     
@@ -36,12 +41,26 @@ def generate_criteria(rough_draft_path: str, final_draft_path: str, model_type: 
     return response_format.model_validate_json(chat_completion).model_dump()
 
 if __name__ == "__main__":
-    # Testing
-    rough_draft_path = os.path.join(os.path.dirname(__file__), "documents/alma_anonymized_draft", "243711d2cc.txt")
-    final_draft_path = os.path.join(os.path.dirname(__file__), "documents/alma_anonymized_final", "243711d2cc.txt")
-    output = generate_criteria(rough_draft_path, final_draft_path)
-    criteria = output["CRITERIA"]
-    thoughts = output["THOUGHTS"]
-    print(f"{criteria=}, {thoughts=}")
-    print("*" * 50)
+    with open("./test.txt", "w") as file:
+        file.write("")
+    
+    alma_documents_path = os.path.join(os.path.dirname(__file__), "documents/alma_anonymized_draft")
+    alma_final_path = os.path.join(os.path.dirname(__file__), "documents/alma_anonymized_final")
+    criteria = ""
+    for draft_document_name in os.listdir(alma_documents_path):
+        rough_draft_path = os.path.join(alma_documents_path, draft_document_name)
+
+        final_draft_path = os.path.join(alma_final_path, draft_document_name)
+
+        if os.path.exists(final_draft_path):
+            output = generate_criteria(rough_draft_path, final_draft_path, criteria)
+            criteria = output["CRITERIA"]
+            thoughts = output["THOUGHTS"]
+            with open("./test.txt", "+a") as file:
+                file.write(f"Criteria:\n{criteria}\n\nThoughts:\n{thoughts}\n")
+                file.write("*" * 50 + "\n")
+            print(f"{criteria=}, {thoughts=}")
+            print("*" * 50)
+        else:
+            print(f"No corresponding file for: {draft_document_name}")
     
