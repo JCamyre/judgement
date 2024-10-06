24a093fcbde303b849d352d547c3a9f8e2ef5363
unknown <alexshanvi@gmail.com>
2024-10-06 13:32:15 -0700

    Further set up use of judge classes

--- judgement/eval/model_graded/judge.py
+++ judgement/eval/model_graded/judge.py
@@ -7,7 +7,8 @@ Assume you have:
 - a criteria for judging the quality of the predicted/gold outputs 
 """
 
-from typing import List, Tuple, Mapping
+from typing import List, Tuple, Mapping, Callable
+from judgement.data.common import dataset 
 
 
 class LLMJudge:
@@ -18,16 +19,34 @@ class LLMJudge:
 
     def __init__(self, judge):
         self.judge = judge        
-        self.eval_prompt = ""  # prompt for the evaluation task  TODO write this
+        self.eval_prompt = ""  # base prompt for the evaluation task  TODO write this
     
-    def evaluate_sample(self, pred, gold, criteria):
+    def evaluate_sample(self, criteria: str, pred: str, gold: str):
         """
         Produces an evaluation of the predicted output against the gold output
         based on the judge's criteria.
+
+        Args:
+            criteria (str): the criteria for evaluation
+            pred (str): the predicted output
+            gold (str): the gold output
+            
         """
+        # TODO 
+        # 1. generate the prompt for the evaluation task
+        # 2. run the judge model on the prompt
         pass 
 
-    def evaluate_test_set(self, dataset):
+    def evaluate_test_set(self, criteria: str, dataset: dataset.ResponseDataset, collate_fn: Callable, batch_size: int):
+        """
+        Produces an evaluation of the predicted outputs against the gold outputs in the dataset.
+
+        Args:
+            criteria (str): the criteria for evaluation
+            dataset (dataset.ResponseDataset): the dataset containing the predicted and gold outputs
+            collate_fn (Callable): the collate function to use for batching the data
+            batch_size (int): the batch size to use for evaluation
+        """
         pass
 
 
@@ -39,11 +58,33 @@ class MixtureofJudges:
 
     def __init__(self, judges: List[str], mixture_prompt: str):
         self.judges = [LLMJudge(model) for model in judges]
+        self.eval_prompt = ""  # base prompt for the evaluation task  TODO write this
         self.mixture_prompt = mixture_prompt
 
-    def evaluate_sample(self, pred, gold):
+    def evaluate_sample(self, pred: str, gold: str, criteria: str):
+        """
+        Produces an evaluation of the predicted output against the gold output based on the judges' criteria.
+
+        Args:
+            pred (str): the predicted output
+            gold (str): the gold output
+            criteria (str): the criteria for evaluation
+        """
+        # TODO
+        # 1. generate the prompt for the evaluation task
+        # 2. run the judge models on the prompt (parallelized)
+        # 3. aggregate the results
         pass
 
-    def evaluate_test_set(self, dataset):
+    def evaluate_test_set(self, criteria: str, dataset: dataset.ResponseDataset, collate_fn: Callable, batch_size: int):
+        """
+        Produces an evaluation of the predicted outputs against the gold outputs in the dataset.
+
+        Args:
+            criteria (str): the criteria for evaluation
+            dataset (dataset.ResponseDataset): the dataset containing the predicted and gold outputs
+            collate_fn (Callable): the collate function to use for batching the data
+            batch_size (int): the batch size to use for evaluation
+        """
         pass
 
